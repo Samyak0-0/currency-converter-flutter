da225b4a6df127006d363a7d124fdc2495f1e915
samyak <samyakmhrzn9841@gmail.com>
2025-10-08 10:43:33 +0545

    stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:  MaterialHomePage(),
+    return const MaterialApp(
+      home:  HomePage(),
     );
   }
 }
--- lib/material_page.dart
+++ lib/material_page.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class MaterialHomePage extends StatelessWidget {
-  MaterialHomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    print("initial state");
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    double result = 0;
-    final TextEditingController textEditingController = TextEditingController();
+    print("build fn called");
+
     final border = OutlineInputBorder(
       borderSide: BorderSide(
         color: Colors.green.shade900,
@@ -28,20 +48,20 @@ class MaterialHomePage extends StatelessWidget {
         // actions: [Text("abc"), Text("xyz"), Text("wsd")],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "0",
-              style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 255, 255, 255),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'NPR ${result != 0 ? result.toStringAsFixed(3) : 0}',
+                style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-              child: TextField(
+              TextField(
                 controller: textEditingController,
                 style: TextStyle(color: Colors.black),
                 keyboardType: const TextInputType.numberWithOptions(
@@ -58,14 +78,15 @@ class MaterialHomePage extends StatelessWidget {
                   enabledBorder: border,
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: ElevatedButton(
+              const SizedBox(height: 10),
+              ElevatedButton(
                 onPressed: () {
                   if (kDebugMode) {
                     print("haha, charade you are!");
-                    result = double.parse(textEditingController.text) * 142.03;
+                    setState(() {
+                      result =
+                          double.parse(textEditingController.text) * 142.03;
+                    });
                   }
                 },
                 style: const ButtonStyle(
@@ -84,8 +105,8 @@ class MaterialHomePage extends StatelessWidget {
                 ),
                 child: const Text("Convert"),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
